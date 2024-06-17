665660e7d8abb542937910e0e417f46e9cded3db
Roman Iork <iork@inbox.ru>
2024-06-17 09:09:21 +0400

    deploying on Render with Hello World!

---
+++ app/src/main/java/hexlet/code/App.java
@@ -0,0 +1,18 @@
+package hexlet.code;
+
+import io.javalin.Javalin;
+
+public class App {
+    public static void main(String[] args) {
+        var app = getApp();
+        app.start(7070);
+    }
+
+    public static Javalin getApp() {
+        var app = Javalin.create(config -> config.bundledPlugins.enableDevLogging());
+        app.get("/", ctx -> {
+            ctx.result("Hello World!");
+        });
+        return app;
+    }
+}
